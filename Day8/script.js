3a8bda5168e3f9323c8d1b7724deab0274423f5c
surafel <surafelmengist2015@gmail.com>
2025-11-24 10:38:06 +0300

    day 8

---
+++ Day8/script.js
@@ -0,0 +1,31 @@
+// day  8 
+// topics
+// Day 8 — 08/03/2018 E.C
+
+// Topic: Advanced JavaScript Concepts To-Do List:
+//  Learn ES6+ features: let/const, arrow functions, template literals
+//  Understand destructuring, spread/rest operators, default parameters
+//  Study asynchronous JavaScript: promises, async/await Mini Project: Rewrite a simple CRUD app frontend using ES6+ features Resources:
+//  MDN JavaScript Guide
+// Day 8 — 08/03/2018 E.C
+// Topic: Advanced JavaScript Concepts To-Do List:
+//  Learn ES6+ features: let/const, arrow functions, template literals
+//  Understand destructuring, spread/rest operators, default parameters
+//  Study asynchronous JavaScript: promises, async/await Mini Project: Rewrite a simple CRUD app frontend using ES6+ features Resources:
+//  MDN JavaScript
+
+
+
+// Guide
+// let us see one by one
+
+
+// 1. let/const
+// what is let and const?
+// let and const are block-scoped variables introduced in ES6.
+// let allows you to declare variables that can be reassigned, while const is used for variables that should not be reassigned after their initial assignment.
+let x = 10; // can be reassigned
+const y = 20; // cannot be reassigned
+const z = 30;
+const w = 50;
+console.log(x, y, z,w)
