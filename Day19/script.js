2b564ef35ecadc050e0de71d494810e77477bada
surafel <surafelmengist2015@gmail.com>
2025-12-11 18:07:03 +0300

    intial commit for day 19 es6

---
+++ Day19/script.js
@@ -0,0 +1,64 @@
+// let age="hello";
+// function checkAge(){
+//     age<5 ? console.log("you are child") : 
+//     age<10   ? console.log("you are tenager")  :
+//     age<18 ?   console.log(" you are young  ")  : 
+//     age<30 ?   console.log(" you are adult" ) :
+//     age<30 ?    console.log(" you are old" ):
+//    console.log("invalid input") 
+
+// }
+// checkAge()
+
+
+
+
+// let reduceNum=number.reduce((a,b) => a + b, 0)
+// console.log(reduceNum)
+
+// // the second dreducer method in normal functon
+// const reducer1=number.reduce(function(b,c){
+    //       return   b+ c
+    // },0)
+    
+    // console.log(reducer1)
+    
+    
+    // foreach() in javascript
+//     const number =[12,34,67,78];
+// number.forEach((num) =>console.log(num))
+
+// // for loop
+// for(let i = 0; i < number;i--){
+//     console.log(i)
+// }
+
+
+// while loop in js
+// let i=0;
+// while(i<10){
+//      console.log("the value of i is "  + i);
+//      i++;
+// }
+
+
+
+
+
+// do while loop in js
+//  let i=0
+// do{
+//     console.log(i)
+// }
+// while(i<10){
+//     i++
+// }
+
+
+// while loop in is
+// let day =5
+// while(day){
+//     case 1:
+//         console.log("")
+// }
+
